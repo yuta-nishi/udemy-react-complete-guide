03ecb6d39cfa5c1c4824cb55a8e1bd8bb8a89eeb
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-24 00:35:17 +0900

    36. Passing Data via "props"

--- section-3/src/App.jsx
+++ section-3/src/App.jsx
@@ -1,12 +1,53 @@
 import ExpenseItem from './components/ExpenseItem';
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2023, 1, 1),
+    },
+    {
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2023, 1, 1),
+    },
+    {
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2023, 1, 1),
+    },
+    {
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2023, 1, 1),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
-}
+};
 
 export default App;
