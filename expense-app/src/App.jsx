3955d853e25ca256aff16a1537c47a4ef1dea575
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-29 14:05:38 +0900

    65. Understanding "Keys"

--- expense-app/src/App.jsx
+++ expense-app/src/App.jsx
@@ -1,34 +1,31 @@
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-const App = () => {
-  const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
-  };
+const DUMMY_EXPENSES = [
+  {
+    id: 'a',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2023, 9, 14),
+  },
+  { id: 'b', title: 'New TV', amount: 799.49, date: new Date(2023, 3, 12) },
+  {
+    id: 'c',
+    title: 'Car Insurance',
+    amount: 294,
+    date: new Date(2023, 3, 28),
+  },
+];
 
-  const expenses = [
-    {
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2023, 1, 1),
-    },
-    {
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2023, 1, 1),
-    },
-    {
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2023, 1, 1),
-    },
-    {
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2023, 1, 1),
-    },
-  ];
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
