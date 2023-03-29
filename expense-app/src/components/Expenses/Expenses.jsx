5907041847fc0d25fd844138a731490724b7ddde
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-29 18:53:38 +0900

    68. Demo App: Adding a Chart

--- expense-app/src/components/Expenses/Expenses.jsx
+++ expense-app/src/components/Expenses/Expenses.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 
@@ -19,6 +20,7 @@ const Expenses = (props) => {
     <li>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </li>
