fbd9da70b87bde1e4f2eece24f3adb8065650c19
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-29 23:13:02 +0900

    80. Dynamic Styles with CSS Modules

--- styling/src/components/CourseGoals/CourseGoalList/CourseGoalList.jsx
+++ styling/src/components/CourseGoals/CourseGoalList/CourseGoalList.jsx
@@ -3,15 +3,11 @@ import React from 'react';
 import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
 import './CourseGoalList.css';
 
-const CourseGoalList = props => {
+const CourseGoalList = (props) => {
   return (
     <ul className="goal-list">
-      {props.items.map(goal => (
-        <CourseGoalItem
-          key={goal.id}
-          id={goal.id}
-          onDelete={props.onDeleteItem}
-        >
+      {props.items.map((goal) => (
+        <CourseGoalItem key={goal.id} id={goal.id} onDelete={props.onDeleteItem}>
           {goal.text}
         </CourseGoalItem>
       ))}
