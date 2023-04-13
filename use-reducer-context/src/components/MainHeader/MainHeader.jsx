7892796f9a1591b5dd5400bde2ecbd4ca9c7eefa
yuta-nishi <ypmtk437.0209@gmail.com>
2023-04-13 16:16:40 +0900

    121. Using the React Context API

--- use-reducer-context/src/components/MainHeader/MainHeader.jsx
+++ use-reducer-context/src/components/MainHeader/MainHeader.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
+import Navigation from './Navigation';
 
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+      <Navigation onLogout={props.onLogout} />
     </header>
   );
 };
