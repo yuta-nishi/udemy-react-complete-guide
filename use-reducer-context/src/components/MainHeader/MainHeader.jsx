47b608b12b7ef3af3decc718b2823f8fcb82eeb7
yuta-nishi <ypmtk437.0209@gmail.com>
2023-04-13 16:30:35 +0900

    123. Making Context Dynamic

--- use-reducer-context/src/components/MainHeader/MainHeader.jsx
+++ use-reducer-context/src/components/MainHeader/MainHeader.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import classes from './MainHeader.module.css';
 import Navigation from './Navigation';
 
-const MainHeader = (props) => {
+const MainHeader = () => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation onLogout={props.onLogout} />
+      <Navigation />
     </header>
   );
 };
--- use-reducer-context/src/components/MainHeader/Navigation.jsx
+++ use-reducer-context/src/components/MainHeader/Navigation.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const context = useContext(AuthContext);
 
   return (
@@ -21,7 +21,7 @@ const Navigation = (props) => {
         )}
         {context.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={context.onLogout}>Logout</button>
           </li>
         )}
       </ul>
