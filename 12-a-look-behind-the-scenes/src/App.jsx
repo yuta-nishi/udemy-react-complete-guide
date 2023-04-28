8499501ba762dcb395d9135f6eaf95655e4e5079
yuta-nishi <ypmtk437.0209@gmail.com>
2023-04-28 16:34:25 +0900

    151. How React Really Works

---
+++ 12-a-look-behind-the-scenes/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+    </div>
+  );
+};
+
+export default App;
