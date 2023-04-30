3ce6e4e813c069102785a614c69e1c5b26655404
yuta-nishi <ypmtk437.0209@gmail.com>
2023-04-30 22:55:30 +0900

    155. Preventing Function Re-Creation with

--- 12-a-look-behind-the-scenes/src/App.jsx
+++ 12-a-look-behind-the-scenes/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -9,9 +9,9 @@ const App = () => {
 
   console.log('APP RUNNING');
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
