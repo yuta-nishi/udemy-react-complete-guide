c2ea549abb16b34d3d67375a4acce15afa09c59c
yuta-nishi <ypmtk437.0209@gmail.com>
2023-04-30 22:41:08 +0900

    153. A Closer Look At Child Component

--- 12-a-look-behind-the-scenes/src/App.jsx
+++ 12-a-look-behind-the-scenes/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 const App = () => {
@@ -15,7 +16,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
