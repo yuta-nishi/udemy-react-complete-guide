7b2b8418b94e564be7212908d85ba12d2c27e7f6
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-04 18:08:27 +0900

    190. Onwards To A More Realistic Example

--- 15-building-custom-react-hooks/src/App.jsx
+++ 15-building-custom-react-hooks/src/App.jsx
@@ -1,11 +1,52 @@
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React, { useEffect, useState } from 'react';
+
+import NewTask from './components/NewTask/NewTask';
+import Tasks from './components/Tasks/Tasks';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://react-http-922cd-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
     <>
-      <ForwardCounter />
-      <BackwardCounter />
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} />
     </>
   );
 }
