9f93a5fbe22a5a4c6b1752f104a42478448bf25d
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-10 22:07:03 +0900

    244. Migrating Everything To Redux Toolkit

--- 18-diving-into-redux/src/components/Counter.jsx
+++ 18-diving-into-redux/src/components/Counter.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 
+import { counterActions } from '../store';
 import classes from './Counter.module.css';
 
 const Counter = () => {
@@ -8,19 +9,19 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 10 });
+    dispatch(counterActions.increase(10));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
