cc6fd2ed539d31d224406ee6135cb1f0cc7c4e25
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-11 00:48:26 +0900

    253. Refresher / Practice: Part 2/2

--- 19-advanced-redux/src/components/Layout/Layout.jsx
+++ 19-advanced-redux/src/components/Layout/Layout.jsx
@@ -1,12 +1,11 @@
-import { Fragment } from 'react';
 import MainHeader from './MainHeader';
 
 const Layout = (props) => {
   return (
-    <Fragment>
+    <>
       <MainHeader />
       <main>{props.children}</main>
-    </Fragment>
+    </>
   );
 };
 
