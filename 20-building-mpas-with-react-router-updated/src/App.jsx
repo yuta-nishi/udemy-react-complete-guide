30c25e39e87efaafe5dafb97fa03cb54c88a2db5
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-11 18:28:26 +0900

    274. Showing Error Pages with errorElement

--- 20-building-mpas-with-react-router-updated/src/App.jsx
+++ 20-building-mpas-with-react-router-updated/src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
