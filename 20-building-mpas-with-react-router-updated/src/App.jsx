0ed5320880130774f53390df2fe3212010a0d0e4
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-11 19:06:18 +0900

    278. Adding Links for Dynamic Routes

--- 20-building-mpas-with-react-router-updated/src/App.jsx
+++ 20-building-mpas-with-react-router-updated/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
+import ProductDetailPage from './pages/ProductDetail';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> },
     ],
   },
 ]);
