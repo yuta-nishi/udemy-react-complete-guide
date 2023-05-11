0f42e524325eb16da2a0b97ba835fb6f2ac24977
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-11 17:41:44 +0900

    269. Defining Routes

--- 20-building-mpas-with-react-router-updated/src/App.jsx
+++ 20-building-mpas-with-react-router-updated/src/App.jsx
@@ -1,5 +1,10 @@
-function App() {
-  return <div></div>;
-}
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
