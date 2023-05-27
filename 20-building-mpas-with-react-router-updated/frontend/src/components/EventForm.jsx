da1b0fe64e0abd6d0fabcb155e5dfcf0c6430afd
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-27 12:50:04 +0900

    302. Reusing Actions via Request Methods

--- 20-building-mpas-with-react-router-updated/frontend/src/components/EventForm.jsx
+++ 20-building-mpas-with-react-router-updated/frontend/src/components/EventForm.jsx
@@ -1,4 +1,11 @@
-import { Form, useActionData, useNavigate, useNavigation } from 'react-router-dom';
+import {
+  Form,
+  json,
+  redirect,
+  useActionData,
+  useNavigate,
+  useNavigation,
+} from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
@@ -14,7 +21,7 @@ const EventForm = ({ method, event }) => {
   }
 
   return (
-    <Form method="post" className={classes.form}>
+    <Form method={method} className={classes.form}>
       {data && data.errors && (
         <ul>
           {Object.values(data.errors).map((error) => (
@@ -73,3 +80,40 @@ const EventForm = ({ method, event }) => {
 };
 
 export default EventForm;
+
+export const action = async ({ request, params }) => {
+  const method = request.method;
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  };
+
+  let url = 'http://localhost:8080/events';
+
+  if (method === 'PATCH') {
+    const eventId = params.eventId;
+    url += `/${eventId}`;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: 'Could not save event.' }, { status: 500 });
+  }
+
+  return redirect('/events');
+};
--- 20-building-mpas-with-react-router-updated/frontend/src/pages/EditEvent.jsx
+++ 20-building-mpas-with-react-router-updated/frontend/src/pages/EditEvent.jsx
@@ -5,7 +5,7 @@ import EventForm from '../components/EventForm';
 const EditEventPage = () => {
   const data = useRouteLoaderData('eventDetail');
 
-  return <EventForm event={data.event} />;
+  return <EventForm method="patch" event={data.event} />;
 };
 
 export default EditEventPage;
