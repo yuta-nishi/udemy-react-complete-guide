157c6bd7dd2a9c7e48d777101462a2520f92dc7c
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-30 11:44:17 +0900

    314. Implementing the Auth Action

--- 21-authentication/frontend/src/App.jsx
+++ 21-authentication/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import { action as manipulateEventAction } from './components/EventForm';
-import AuthenticationPage from './pages/Authentication';
+import AuthenticationPage, { action as authAction } from './pages/Authentication';
 import EditEventPage from './pages/EditEvent';
 import ErrorPage from './pages/Error';
 import EventDetailPage, {
@@ -63,6 +63,7 @@ const router = createBrowserRouter([
       {
         path: 'auth',
         element: <AuthenticationPage />,
+        action: authAction,
       },
     ],
   },
