40c6fec3bfe4db1822bbf3b1d57811a2033a42bc
yuta-nishi <ypmtk437.0209@gmail.com>
2023-05-30 12:17:36 +0900

    318. Adding User Logout

--- 21-authentication/frontend/src/App.jsx
+++ 21-authentication/frontend/src/App.jsx
@@ -11,6 +11,7 @@ import EventDetailPage, {
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
 import HomePage from './pages/Home';
+import { action as logoutAction } from './pages/Logout';
 import NewEventPage from './pages/NewEvent';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import RootLayout from './pages/Root';
@@ -64,6 +65,10 @@ const router = createBrowserRouter([
         path: 'auth',
         element: <AuthenticationPage />,
         action: authAction,
+      },
+      {
+        path: 'logout',
+        action: logoutAction,
       },
     ],
   },
--- 21-authentication/frontend/src/components/MainNavigation.jsx
+++ 21-authentication/frontend/src/components/MainNavigation.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Form, NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import NewsletterSignup from './NewsletterSignup';
@@ -40,6 +40,11 @@ function MainNavigation() {
             >
               Authentication
             </NavLink>
+          </li>
+          <li>
+            <Form method="POST" action="/logout">
+              <button>Logout</button>
+            </Form>
           </li>
         </ul>
       </nav>
