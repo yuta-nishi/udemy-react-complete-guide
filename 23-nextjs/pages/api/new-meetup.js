8e470a7c1fb6d73e04789f25a381bc35eef3c66b
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-01 18:02:07 +0900

    359. Working with MongoDB

---
+++ 23-nextjs/pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://yutanishi:V83nMtFtATM8wgIU@nextjs.683pzae.mongodb.net/?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
