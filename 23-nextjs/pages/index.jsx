3d8636878c97e957243c7a785a8f89ebeb84f49b
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-02 22:02:39 +0900

    363. Adding "head" Metadata

--- 23-nextjs/pages/index.jsx
+++ 23-nextjs/pages/index.jsx
@@ -1,9 +1,21 @@
 import { MongoClient } from 'mongodb';
+import Head from 'next/head';
 
 import MeetupList from '@/components/meetups/MeetupList';
 
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <>
+      <Head>
+        <title>React meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
 }
 
 // export async function getServerSideProps(context) {
--- 23-nextjs/pages/new-meetup/index.jsx
+++ 23-nextjs/pages/new-meetup/index.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import NewMeetupForm from '@/components/meetups/NewMeetupForm';
@@ -21,7 +22,18 @@ function NewMeetupPage() {
     router.push('/');
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
 }
 
 export default NewMeetupPage;
