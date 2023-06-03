6c97f15d48064da86a2a121383435ceefcc7ebbe
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-03 21:14:56 +0900

    409. Testing Asynchronous Code

--- 26-testing/src/App.jsx
+++ 26-testing/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Async from './components/Async';
 import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
+      <Async />
       <Greeting />
     </div>
   );
