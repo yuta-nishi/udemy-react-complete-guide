efd499bca0f4c06b1e8005b57c57b690a1d59622
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-04 00:49:55 +0900

    433. Adding Styling

--- 27-react-typescript/src/components/NewTodo.tsx
+++ 27-react-typescript/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
 import { useRef } from 'react';
 
+import classes from './NewTodo.module.css';
+
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
@@ -15,7 +17,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
