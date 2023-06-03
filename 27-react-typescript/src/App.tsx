372424d1b2914d98a0a84654673b5c9331155037
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-04 01:05:04 +0900

    434. Time to Practice: Removing a Todo

--- 27-react-typescript/src/App.tsx
+++ 27-react-typescript/src/App.tsx
@@ -15,10 +15,16 @@ const App = () => {
     });
   };
 
+  const removeHandler = (todoId: string): void => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeHandler} />
     </div>
   );
 };
