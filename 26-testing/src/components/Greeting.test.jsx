eaf49eb7408e1c178eb453605998de28c3b507ac
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-03 18:45:14 +0900

    407. Testing User Interaction & State

--- 26-testing/src/components/Greeting.test.jsx
+++ 26-testing/src/components/Greeting.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Greeting from './Greeting';
 
@@ -13,5 +14,32 @@ describe('Greeting component', () => {
     // Assert
     const helloWorldElement = screen.getByText('Hello World!');
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('render "good to see you" if the button was NOT clicked', () => {
+    render(<Greeting />);
+
+    const helloWorldElement = screen.getByText('good to see you', { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('render "Changed!" if the button was clicked', () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const helloWorldElement = screen.getByText('Changed!');
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const helloWorldElement = screen.queryByText('good to see you', { exact: false });
+    expect(helloWorldElement).toBeNull();
   });
 });
