d7bd09299acf8d4940c5e3a349f1f92dbd9c2021
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-03 23:50:59 +0900

    431. Working with "Function Props"

--- 27-react-typescript/src/App.tsx
+++ 27-react-typescript/src/App.tsx
@@ -1,11 +1,15 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/Todo';
 
 const App = () => {
   const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
 
+  const addTodoHandler = (todoText: string): void => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- 27-react-typescript/src/components/NewTodo.tsx
+++ 27-react-typescript/src/components/NewTodo.tsx
@@ -1,15 +1,17 @@
 import { useRef } from 'react';
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
-  const submitHandler = (event: React.FormEvent) => {
+  const submitHandler = (event: React.FormEvent): void => {
     event.preventDefault();
 
     const enteredText = todoTextInputRef.current!.value;
     if (enteredText.trim().length === 0) {
       return;
     }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
